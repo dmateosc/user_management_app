e0c3864ad846a8c998e79641a0b194058248ca75
dmateosc <matesocano.david@gmail.com>
2024-05-18 01:04:56 +0200

    afirst commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Gym App
+      </header>
+      <main>
+      </main>
+    </div>
+  );
+}
+
+export default App;
